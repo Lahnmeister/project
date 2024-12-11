d302d8cea096d0d0974f968d760225492755d7c3
fdai7172 <leon.doehrer@informatik.hs-fulda.de>
2024-12-11 18:00:57 +0100

    added Overview window of my measured trees

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,11 +2,7 @@
     <nav :class="['header', darkMode ? 'dark' : 'light']">
       <ul>
         <li><router-link to="/">Startseite</router-link></li>
-        <!--<li><router-link to="/trivia">Trivia</router-link></li>
-        <li><router-link to="/vorteile">Vorteile</router-link></li>
-        <li><router-link to="/MobileApp">Mobile App</router-link></li>
-        <li><router-link to="/v-model">Demo - v-model</router-link></li>
-        <li><router-link to="/testapp">Testapp</router-link></li>-->
+        <li><router-link to="/overview">Übersicht</router-link></li>
       </ul>
       <label class="switch">
         <input type="checkbox" v-model="darkMode" @change="toggleDarkMode" />
--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-    { path: '/', component: () => import('./components/testapp.vue') }
+    { path: '/', component: () => import('./components/testapp.vue') },
+    { path: '/overview', component: () => import('./components/OverviewComponent.vue') }
 ];
 
 const router = createRouter({
